490ee58c2e18ad3b28148ebc3918ad55f32c2f3a
Kunsguru <kingsleym2024@gmail.com>
2024-06-15 14:06:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -472,29 +472,6 @@ document.querySelector('.next').addEventListener('click', () => {
 });
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('summitButton1').addEventListener('click', function() {
-        var email1 = 'mondaykingsley80@gmail.com';
-        var subject1 = 'Summit Request for Button 1';
-        var body1 = 'Hello,\n\nI would like to request a summit.\n\nThank you.';
-        
-        var mailtoLink1 = 'mailto:' + email1 + '?subject=' + encodeURIComponent(subject1) + '&body=' + encodeURIComponent(body1);
-        window.location.href = mailtoLink1;
-    });
-
-    document.getElementById('summitButton2').addEventListener('click', function() {
-        var email2 = 'artsbasseyartsbassey@gmail.com';
-        var subject2 = 'Summit Request for Button 2';
-        var body2 = 'Hello,\n\nI would like to request a summit.\n\nThank you.';
-        
-        var mailtoLink2 = 'mailto:' + email2 + '?subject=' + encodeURIComponent(subject2) + '&body=' + encodeURIComponent(body2);
-        window.location.href = mailtoLink2;
-    });
-});
-
-
-
-
 
 
                     function handleClick() {
@@ -506,12 +483,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function handleRegistrationClick() {
     alert('Redirecting You, Our dearest customer to our Official Web Form Page ...');
-    window.location.href = 'https://arts-with-artsbassey-registration-form.onrender.com';
+    window.location.href = 'https://project12345.onrender.com/';
 }
 
 function handleContactClick() {
     alert('Redirecting You, Our dearest customer to our Official Web Contact Form Page ...');
-    window.location.href = 'https://artswithartsybasy-contact-form.onrender.com';
+    window.location.href = 'https://project12345.onrender.com/';
 }                                                                                                      
             
                 
