8025556fff9d6e8803bf158a7159d346698fa764
Kunsguru <kingsleym2024@gmail.com>
2024-06-15 14:44:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -474,11 +474,7 @@ document.querySelector('.next').addEventListener('click', () => {
 
 
 
-                    function handleClick() {
-    alert('Redirecting You, Our dearest customer to our Official Web Form Page ...');
-    window.location.href = 'https://arts-with-artsbassey-registration-form.onrender.com';
-}
-
+                    
 
 
 function handleRegistrationClick() {
